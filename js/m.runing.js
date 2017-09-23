65042ddc3a47493129297b7c0c7047b6226cff67
liubin <752791962@qq.com>
2017-09-23 10:06:23 +0800

    12345

--- js/m.runing.js
+++ js/m.runing.js
@@ -8,4 +8,17 @@ $(function(){
   $('div.details_push_btn>button').tap(function(){
 		$('section.details_push_tip').show();
 	})
+  $('.details_push_change span.close').tap(function(){
+				$(this).addClass('hidden');
+				$('section.newCar_details_share').css({'padding-bottom': '1.2rem'});
+				$(this).parents('.details_push').animate({'right': '-100%'}, 300, function(){
+					$(this).parent().find('span.open').removeClass('hidden').addClass('details_push_change_fixed');
+				});
+	});
+	$('.details_push_change span.open').tap(function(){
+				$(this).addClass('hidden');
+				$(this).parents('.details_push').css({'right': 0});
+				$(this).parent().find('span.close').removeClass('hidden details_push_change_fixed');
+				$('section.newCar_details_share').css({'padding-bottom': '4.2rem'});
+	})
 })
